3d6b144d992d378270df9914a8e4d2d2c91d5962
Aswin-Dot <56764210+Aswin-Dot@users.noreply.github.com>
2021-01-25 22:17:38 +0530

    final commit

---
+++ public/Resources/js/description.js
@@ -0,0 +1,41 @@
+/* Preloader */
+$(window).on('load', function() {
+    var preloaderFadeOutTime = 1000;
+    function hidePreloader() {
+        var preloader = $('.spinner-wrapper');
+        setTimeout(function() {
+            preloader.fadeOut(preloaderFadeOutTime);
+        }, 1000);
+    }
+    hidePreloader();
+});
+
+/* store */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
